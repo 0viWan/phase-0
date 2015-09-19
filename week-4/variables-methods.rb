1aed9ebc6838a6ff091735acaf26dcc167c2fc92
Ovidio Calvo <OviWan@Ovidios-MacBook-Air.local>
2015-09-19 08:42:01 -0700

    Add gps2.2

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -11,3 +11,16 @@ puts "Hey stranger, wanna tell me your favorite number?"
 fav_num = gets.chomp.to_i
 better_number = fav_num + 1
 puts "But " + better_number.to_s + " is such a bigger and better number! Maybe this should be your fave.."
+
+# How do you define a local variable?
+=begin A local variable is defined initially by a lower case letter or an underscore
+proceeded by the rest of the word, and followed by an equal sign. For example ,
+' my_name = "Ovi" ' is how you can define a variable.'
+=end
+
+# How do you define a method?
+=begin A method is defind
+# What is the difference between a local variable and a method?
+#How do you run a ruby program from the command line?
+#How do you run an RSpec file from the command line?
+#What was confusing about this material? What made sense?
