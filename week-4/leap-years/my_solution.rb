fc3acdd3af651c11891fb87b06e15962ab79b9bd
Ovidio Calvo <OviWan@Ovidios-MacBook-Air.local>
2015-09-30 16:30:55 -0700

    Add leap year exercise

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -2,16 +2,33 @@
 # PSEDUOCODE: -Method that accepts an integer as input representative of a year
 # return 'true' if the year is in fact a leap year and 'false' if not.
 =begin  **Research**
-1) If the year is evenly divisible by 4, go to step 2. ...
-2) If the year is evenly divisible by 100, go to step 3. ...
-3) If the year is evenly divisible by 400, go to step 4. ...
-4) The year is a leap year (it has 366 days).
-5)The year is not a leap year (it has 365 days).
+1)The year is evenly divisible by 4;
+2)If the year can be evenly divided by 100, it is NOT a leap year, unless;
+3)The year is also evenly divisible by 400. Then it is a leap year.
+
+ "There is a leap year every year whose number is perfectly divisible by four
+ - except for years which are both divisible by 100 and not divisible by 400. 
+ The second part of the rule effects century years. 
+ For example; the century years 1600 and 2000 are leap years, 
+ but the century years 1700, 1800, and 1900 are not. This means that 
+three times out of every four hundred years there are eight years between 
+leap years"
+
 =end
 
 # I worked on this challenge [by myself].
-def leap_year?(year)
-  if ((year/4) = 0
-
 
 # Your Solution Below
+
+def leap_year?(year)
+  if year % 400 == 0
+  	return true
+  elsif year % 100 == 0
+  	return false
+  elsif year % 4 == 0
+  	return true
+  else
+  	return false
+  end
+end
+
