5e37938295754cb7f5d4ce09bf04c004ab3eb1f1
Ovidio Calvo <OviWan@Ovidios-MacBook-Air.local>
2015-09-30 11:17:39 -0700

    Add pairing exercises and corrections

--- week-4/calculate-grade/my_solution.rb
+++ week-4/calculate-grade/my_solution.rb
@@ -1,10 +1,30 @@
 # Calculate a Grade
 
-# I worked on this challenge [by myself, with: ].
-
-
+# I worked on this challenge [with: Kyle Smith ].
+# method get_grade that accepts and average (integer)
+# and returns a string of a letter grade (ie A,C,F)
+# do not worry about + and - grading
 # Your Solution Below
 
+def get_grade(grade)
+	if grade >= 90
+		return "A"
+
+	elsif grade >= 80 
+		return "B"
+
+	elsif grade >= 70 
+		return "C"
+
+	elsif grade >= 60 
+		return "D"
+
+	else
+		return "F"
+	end
+end
+
+=begin			(*this is an alternate solution I came up with that passes the test but it more verbose*)
 def get_grade(grade)
 	if grade >= 90
 		return "A"
@@ -22,7 +42,5 @@ def get_grade(grade)
 		return "F"
 	end
 end
-
-
-
+=end
 
