c5acd655ed32854b03160b9f59b9a9bfe4124d05
Ovidio Calvo <OviWan@Ovidios-MacBook-Air.local>
2015-10-05 08:17:00 -0700

    Add exercises again

--- week-4/concatenate-arrays/my_solution.rb
+++ week-4/concatenate-arrays/my_solution.rb
@@ -1,10 +1,17 @@
 # Concatenate Two Arrays
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # Your Solution Below
+=begin
+def array_concat(array_1, array_2)
+  return array_1 + array_2
+end
+=end
 
 def array_concat(array_1, array_2)
-  # Your code here
+	array_1.push (array_2)
+	return array_1.flatten
 end
+
